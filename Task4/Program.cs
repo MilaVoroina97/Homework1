158f791218a3bfd06e6b35d0d832097d112bc8cc
mila <milavoronina1997@mail.ru>
2022-08-24 16:37:15 +0500

    Task4_solve

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i<=a; i++)
+{
+    if (i % 2 ==0)
+    {
+        Console.Write(i + " ");
+    }
+
+}
