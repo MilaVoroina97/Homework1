e291b5316316648fa350e238f0ff13b916b16aae
mila <milavoronina1997@mail.ru>
2022-08-24 18:29:45 +0500

    Task5_solve

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -2,18 +2,18 @@
 
 // 1 вариант 
 
-//Console.WriteLine("Введите целое  число: ");
-//string x = Console.ReadLine();
+Console.WriteLine("Введите целое  число: ");
+string x = Console.ReadLine();
 
-//var result = x.Skip(1).Take(1).FirstOrDefault();
-//Console.Write(result); // => 1
+var result = x.Skip(1).Take(1).FirstOrDefault();
+Console.Write(result); // => 1
 
 
 // 2 вариант
 
-Console.WriteLine("Введите целое  число: ");
-string x = Console.ReadLine(); 
-int y = int.Parse(x);
-string a = x.Substring(1,1); //диапозон обрезки индекс начала обрезки и длину вырезаемой части строки.
-if (y > 9) Console.WriteLine(a);
-else Console.WriteLine("enter number, which is more than 10");
+//Console.WriteLine("Введите целое  число: ");
+//string x = Console.ReadLine(); 
+//int y = int.Parse(x);
+//string a = x.Substring(1,1); //диапозон обрезки индекс начала обрезки и длину вырезаемой части строки.
+//if (y > 9) Console.WriteLine(a);
+//else Console.WriteLine("enter number, which is more than 10");
