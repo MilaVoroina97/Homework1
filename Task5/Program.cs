f70148d8366380a3ab23855b5502600bd1e7246c
mila <milavoronina1997@mail.ru>
2022-08-24 16:44:10 +0500

    Task5_solve

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Доп. задание, которое было дано на семинаре. Вывести второе число слева направо в любом вводимом целом числе 
+
+// 1 вариант 
+
+//Console.WriteLine("Введите целое  число: ");
+//string x = Console.ReadLine();
+
+//var result = x.Skip(1).Take(1).FirstOrDefault();
+//Console.Write(result); // => 1
+
+
+// 2 вариант
+
+Console.WriteLine("Введите целое  число: ");
+string x = Console.ReadLine(); 
+int y = int.Parse(x);
+string a = x.Substring(1,1); //диапозон обрезки индекс начала обрезки и длину вырезаемой части строки.
+if (y > 9) Console.WriteLine(a);
+else Console.WriteLine("enter number, which is more than 10");
