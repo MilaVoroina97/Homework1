d8c444e2db26f9f79bc51e8ec134b85e2387dc2a
mila <milavoronina1997@mail.ru>
2022-08-25 22:59:42 +0500

    test2

--- test/Program.cs
+++ test/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа
+
+int number = new Random(). Next(10,100);
+Console.WriteLine(number);
+string y = number.ToString();
+if (y[0] > y[1])
+{
+    Console.WriteLine(y[0]);
+}
+else
+{
+    if (y[0]< y[1])
+    {
+        Console.WriteLine(y[1]);
+    }
+   
+}
