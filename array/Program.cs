d3d9156367a3aa4e966590244e8e8a2af4c6dc2c
mila <milavoronina1997@mail.ru>
2022-08-25 16:50:32 +0500

    array

---
+++ array/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2  > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[]array = {1,2,3,4,5,65,7,8,9};
+
+
+int max = Max(
+    Max(array[0],array[1],array[2]),
+    Max(array[3],array[4],array[5]),
+    Max(array[6],array[7],array[8]));
+
+Console.WriteLine(max);
+   
+
+//int max = Max( 
+    //Max(a1,a2,a3),
+    //Max(a4,a5,a6),
+    //Max(a7,a8,a9));
+
+//Console.WriteLine(max);
