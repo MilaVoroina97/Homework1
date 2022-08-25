8e884debee767e019efaaf22dae0b597719e26b6
mila <milavoronina1997@mail.ru>
2022-08-25 17:16:32 +0500

    array2

---
+++ array2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[]massiv = {1,12,31,41,15,16,17,18,15,10,15};
+
+int n = massiv.Length;
+int find = 15;
+int index = 0;
+while (index < n)
+{
+    if (massiv[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
