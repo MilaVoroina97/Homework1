accb688d33e8442a9e58013ace0e4db975ba5ca4
mila <milavoronina1997@mail.ru>
2022-08-26 16:55:37 +0500

    kvadrat

---
+++ kvadrat/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+Console.WriteLine(" Введите число N :");
+int n = Convert.ToInt32( Console.ReadLine());
+int[] kvadrat = new int[n];
+int index = 1;
+while (index <= n)
+{
+    kvadrat[index - 1] = (index* index);
+    index++;
+}
+var str = string.Join(" ",kvadrat);
+Console.WriteLine(str);
