cdfa11743fc42439e888c5fc555d099f7970a10d
mila <milavoronina1997@mail.ru>
2022-08-29 12:27:27 +0500

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+int[]arr = {5,6,7,99,11,100,55,4};
+//foreach (int i in arr) Console.Write($"{i} ,");
+for(int i = 0; i < arr.Length;i++)
+{
+     Console.Write($"{arr[i]} ,");
+    
+}
+ Console.WriteLine();
+ Console.WriteLine("I need number with index 9. This is : " + arr[3]);
+
